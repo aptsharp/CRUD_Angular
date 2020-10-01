d1e71743187301ddfd73fda8bd08be2436099291
Afonso -aptsharp <aff-on@hotmail.com>
2020-10-01 17:47:50 -0300

    Implementado a navegação lateral ( nav-bar)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,18 +10,25 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
