52956e2c4633032bd438d91cc91b1205ff9eacca
Afonso -aptsharp <aff-on@hotmail.com>
2020-10-02 12:25:08 -0300

    Problemas com rotas

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router'
 
 @Component({
   selector: 'app-product-crud',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Route) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToProductCreate(): void {
+    console.log('teste')
   }
 
 }
