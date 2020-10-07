999995b3d39762b5276d77a1cc4c1401e1b37fe7
Afonso -aptsharp <aff-on@hotmail.com>
2020-10-06 22:57:27 -0300

    Criação de array do back end (lista)

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -26,6 +26,10 @@ export class ProductService {
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product)
   }
+
+  read(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseUrl)
+  }
 }
 
 
