3fa3719777b72444ca73f77615aed6fa001b613d
Afonso -aptsharp <aff-on@hotmail.com>
2020-10-08 20:06:00 -0300

    Acrescentando metodo de delete

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,7 +1,8 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
+import { subscribeOn } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-delete',
@@ -12,12 +13,22 @@ export class ProductDeleteComponent implements OnInit {
 
   product: Product
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {this.product = this.product});
+   }
 
   cancel(): void {
     this.router.navigate(['/product'])
+  }
+
+  deleteProduct(): void{
+    this.productService.delete('id').subscribe(() => {
+      this.productService.showMessage("Deletado")
+      this.router.navigate(['/product'])
+    })
   }
 
 }
